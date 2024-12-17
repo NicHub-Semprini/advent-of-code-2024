04cbf026725dccabc062ae46fed1de03c1489813
Nicola Semprini <nsemprini@gmv.com>
2024-12-17 20:31:00 +0100

    day-9 ex2 ok

--- day-9/Main92.java
+++ day-9/Main92.java
@@ -2,11 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main92 {
     public static void main(String... args) {
-        String fileName = "day-9/input-small.txt";
+        String fileName = "day-9/input9.txt";
 
         // Read input
         int blocks = 0;
@@ -21,31 +20,23 @@ public class Main92 {
         }
 
         // Expand compact memory representation
-        char[] expanded = new char[blocks];
-        List<File> files = new ArrayList<>(); // fileId in descending order
-        Map<Integer, Integer> emptySpaceSizeMap = new TreeMap<>(Comparator.naturalOrder()); // empty space in ascending order
+        int[] expanded = new int[blocks];
+        int fileId = 0;
         try (BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)))) {
             int i = 0;
             int n;
             int pos = 0;
-            int fileId = 48;
             while((n = bf.read()) != 10) {
                 int j;
                 for (j = 0; j < n - 48; j++) {
                     if (pos % 2 == 0) {
-                        expanded[i + j] = (char) fileId;
+                        expanded[i + j] = fileId;
                     } else {
-                        expanded[i + j] = '.';
+                        expanded[i + j] = -1;
                     }
                 }
-                if (pos % 2 == 0) {
-                    files.add(new File((char) fileId, n - 48, i));
+                if (pos % 2 != 0) {
                     fileId++;
-                } else {
-                    if (n != 48) {
-                        // Take note of empty space starting position and size
-                        emptySpaceSizeMap.put(i, n - 48);
-                    }
                 }
                 i = i + j;
                 pos++;
@@ -55,72 +46,30 @@ public class Main92 {
             e.printStackTrace();
         }
 
-        // Sort file by ID descending
-        files.sort(Comparator.comparing(File::getFileId).reversed());
-
-        System.out.println(expanded);
-        System.out.println(files);
-        System.out.println(emptySpaceSizeMap);
-
         // Move blocks
-        for (int i = 0; i < expanded.length; i++) {
+        for (int i = fileId; i >= 0; i--) {
+            int lastFileBlock = getIndexOfLastFileBlock(expanded, i);
 
-            // All files have been moved once, stop looping
-            if (files.isEmpty()) {
-                break;
+            // Never trust
+            if (lastFileBlock == -1) {
+                continue;
             }
 
-            int lastFileBlock = getIndexOfLastFileBlock(expanded);
+            int blockStarting = getStartingIndexOfBlock(expanded, lastFileBlock);
+            int blockSize = lastFileBlock - blockStarting + 1;
 
-            // All blocks have been compacted, stop looping
-            if (lastFileBlock <= i) {
-                break;
+            int blankStarting = findWhiteSpace(expanded, blockStarting, blockSize);
+
+            // Some files cannot be moved
+            if (blankStarting == -1) {
+                continue;
             }
 
-            // Replace only empty memory
-            if (expanded[i] == '.') {
-                int emptySpaceSize = emptySpaceSizeMap.get(i);
-                Optional<File> fileOpt = files.stream().filter(f -> f.size <= emptySpaceSize).findFirst();
-
-                // There is enough space for a file
-                if (fileOpt.isPresent()) {
-                    File file = fileOpt.get();
-
-                    // Move file
-                    for (int j = i; j - i < file.size; j++) {
-                        expanded[j] = file.fileId;
-                    }
-                    files.remove(file);
-
-                    // Fill old file position with blanks
-                    for (int j = file.startingBlock; j - file.startingBlock < file.size; j++) {
-                        expanded[j] = '.';
-                    }
-
-                    // Update blanks map
-                    emptySpaceSizeMap.remove(i);
-                    // Check remaining empty space
-                    if (file.size < emptySpaceSize) {
-                        int nextEmptyStarting = i + file.size;
-                        // Check if the next empty space is behind the corner
-                        if (emptySpaceSizeMap.containsKey(nextEmptyStarting + 1)) {
-                            // Merge empty spaces
-                            int nextEmptySize = emptySpaceSizeMap.remove(nextEmptyStarting + 1);
-                            emptySpaceSizeMap.put(nextEmptyStarting, emptySpaceSize - file.size + nextEmptySize);
-                        } else {
-                            emptySpaceSizeMap.put(i + file.size, emptySpaceSize - file.size);
-                        }
-                    }
-                    // Check old space file
-                    if (emptySpaceSizeMap.containsKey(file.startingBlock + file.size + 1)) {
-                        // Merge empty spaces
-                        int nextEmptySize = emptySpaceSizeMap.remove(file.startingBlock + file.size + 1);
-                        emptySpaceSizeMap.put(file.startingBlock, file.size + nextEmptySize);
-                    } else {
-                        emptySpaceSizeMap.put(file.startingBlock, file.size);
-                    }
-                }
-                System.out.println(expanded);
+            for (int j = 0; j < blockSize; j++) {
+                // Replace blank with file
+                expanded[blankStarting + j] = i;
+                // Replace file with blank
+                expanded[blockStarting + j] = -1;
             }
         }
 
@@ -129,9 +78,9 @@ public class Main92 {
         System.out.println("Checksum: " + checksum);
     }
 
-    public static int getIndexOfLastFileBlock(char[] input) {
+    public static int getIndexOfLastFileBlock(int[] input) {
         for (int i = input.length - 1; i >= 0; i--) {
-            if (input[i] != '.') {
+            if (input[i] != -1) {
                 return i;
             }
         }
@@ -139,38 +88,63 @@ public class Main92 {
         return -1;
     }
 
-    public static long computeChecksum(char[] input) {
+    public static int getIndexOfLastFileBlock(int[] input, int target) {
+        for (int i = input.length - 1; i >= 0; i--) {
+            if (input[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static int getStartingIndexOfBlock(int[] input, int lastIndexOfBlock) {
+        int target = input[lastIndexOfBlock];
+
+        for (int i = lastIndexOfBlock -1; i >= 0; i--) {
+            if (input[i] != target) {
+                return i + 1;
+            }
+        }
+         return -1;
+    }
+
+    public static int findWhiteSpace(int[] input, int maxIndex, int size) {
+        int index = -1;
+        int count = 0;
+
+        // Scan input from the beginning
+        for (int i = 0; i < maxIndex; i++) {
+            if (input[i] == -1) {
+                // Keep track of blank size
+                count++;
+                if (index == -1) { // Blank region started
+                    index = i;
+                }
+            } else { // Reset region
+                index = -1;
+                count = 0;
+            }
+
+            // Blank region is large enough, exit
+            if (count == size) {
+                return index;
+            }
+        }
+
+        return -1;
+    }
+
+    public static long computeChecksum(int[] input) {
         long checksum = 0;
         int lastFileBlock = getIndexOfLastFileBlock(input);
         for (int i = 0; i <= lastFileBlock; i++) {
-            checksum = checksum + i * (input[i] - 48L);
+            // Skip blanks
+            if (input[i] != -1) {
+                checksum = checksum + i * input[i];
+            }
         }
 
         return checksum;
-    }
-
-    public static class File {
-        char fileId;
-        int size;
-        int startingBlock;
-
-        public File(char fileId, int size, int startingBlock) {
-            this.fileId = fileId;
-            this.size = size;
-            this.startingBlock = startingBlock;
-        }
-
-        public char getFileId() {
-            return this.fileId;
-        }
-
-        @Override
-        public String toString() {
-            return "File{" +
-                    "fileId=" + fileId +
-                    ", size=" + size +
-                    ", startingBlock=" + startingBlock +
-                    '}';
-        }
     }
 }
